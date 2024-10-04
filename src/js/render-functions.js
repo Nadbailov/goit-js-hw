cebee1e20d40974b9dfd669248c3c2a5b42f60e9
Nadbailov <nadbailov@gmail.com>
2024-10-04 12:49:33 +0300

    hw12

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -9,6 +9,7 @@ const lightbox = new SimpleLightbox('.gallery-list a', {
      
 export function createImages(data) {
     
+    
     const list = document.querySelector(".gallery-list");
     let images =  data.hits.map((hit) => `<div class="image-frame"><a href="${hit.largeImageURL}"><img class="image" src="${hit.webformatURL}" alt="${hit.tags}" /></a><div class ="text-wraper"><div class="text-block"><h5>likes</h5><p>${hit.likes}</p></div><div class="text-block"><h5>views</h5><p>${hit.views}</p></div><div class="text-block"><h5>comments</h5><p>${hit.comments}</p></div><div class="text-block"><h5>downloads</h5><p>${hit.downloads}</p></div></div></div>`).join("");
     list.insertAdjacentHTML("beforeend", images);
